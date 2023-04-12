0497759ce981445a3063332d3ed5be340330faaa
BarteQ343 <bartoszchmiel72@gmail.com>
2023-04-12 19:49:08 +0200

    Trzeci myślnik

--- skrypt.sh
+++ skrypt.sh
@@ -6,3 +6,16 @@ if [ "$1" = "--date" ]; then
 else
   echo "Użycie: $0 --date"
 fi
+
+if [ "$1" = "--logs" ]; then
+  for i in {1..100}; do
+    filename="log$i.txt"
+    echo "Nazwa pliku: $filename" > $filename
+    echo "Nazwa skryptu: $0" >> $filename
+    echo "Data: $(date +%Y-%m-%d)" >> $filename
+  done
+  echo "Utworzono 100 plików logx.txt"
+else
+  echo "Użycie: $0 --logs"
+fi
+
